b3b3d704adc5e08fd40e36215dd4491bfd43335e
vivekmediware <35924721+vivekmediware@users.noreply.github.com>
2020-07-11 17:58:29 +0530

    Test change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
